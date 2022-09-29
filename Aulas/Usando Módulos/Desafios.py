45ac0957bb9b026403464cd3e7263e277a4ebc8a
Eduardo <edugc20142hotmail.com>
2022-09-29 17:47:46 -0300

    Módulo 8 - Ainda falta terminar os desafios

--- Aulas/Usando Módulos/Desafios.py
+++ Aulas/Usando Módulos/Desafios.py
@@ -1,7 +1,9 @@
 import math
 import random
 import time
-from pygame import mixer
+#from pygame import mixer
+
+#___________________________________________________________________________________________________#
 
 # Crie um programa que leia um número real qualquer pelo teclado e mostre na tela sua porção inteira
 # Exemplo: Digite o número: 6.127 / O número 6.127, tem a parte inteira 6
@@ -14,6 +16,7 @@ from pygame import mixer
 # real = float(input("Digite um valor: "))
 # print(f'O valor informado {real} tem a sua porção inteira igual a {math.trunc(real)}')
 
+#___________________________________________________________________________________________________#
 
 # Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um Triângulo retângulo
 # calcule e mostre o comprimento da hipotenusa.
@@ -31,6 +34,7 @@ from pygame import mixer
 # h1 = (co ** 2 + ca ** 2) ** 0.5
 # print(f'A hipotenusa vai medir {h1:.2f}')
 
+#___________________________________________________________________________________________________#
 
 # Faça um programa que leia o ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente deste ângulo.
 
@@ -47,12 +51,16 @@ from pygame import mixer
 
 # Eixo vertical = seno/Eixo horizontal = cosseno
 
+#___________________________________________________________________________________________________#
+
 # Um professor quer sortear um de seus 4 alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo o nome do escolhido
 # listAluno = []
 # for c in range(4):
 #     aluno = input("Digite o nome do aluno: ")
 #     listAluno.append(aluno)
 # print(random.choice(listAluno))
+
+#___________________________________________________________________________________________________#
 
 # O mesmo professor quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que
 # Leia o nome dos 4 alunos e mostrem a ordem sorteada.
@@ -73,6 +81,8 @@ from pygame import mixer
 #
 # print(listAluno)
 
+#___________________________________________________________________________________________________#
+
 # Faça um programa que abra e reproduza o áudio de um arquivo MP3.
 
 # mixer.init()
@@ -81,18 +91,33 @@ from pygame import mixer
 # musica.set_volume(0.5)
 # time.sleep(150)
 
+
+#___________________________________________________________________________________________________#
 # Crie um programa que leia o nome completo de uma pessoa e mostre:
 
+# nome = input('Digite seu nome: ')
+
 # O nome com todas as letras maiúsculas;
-#
+# print(nome.upper())
+
 # O nome com todas as letras minúsculas;
+# print(nome.lower())
+
+# Quantas letras ao todos sem considerar os espaços;
+# res = 0
+# for c in nome.split():
+#     res += len(c)
 #
-# Qunatas letras ao todos sem considerar os espaços;
-#
-# Qunatas letras tem o primeiro nome;
+# print("Quantidade de letras em seu nome: ",res)
+
+# Quantas letras tem o primeiro nome;
+# nome1 = nome.split()
+# res = len(nome1[0])
+# print(f'Quantidade de letras no primeiro nome: {nome1} = ',res)
+#___________________________________________________________________________________________________#
 
 # Faça um programa que leia números de 0 a 9999 e mostre na tela cada um dos dígitos separados
-#
+
 # Ex:
 # Digite um número: 1834
 #
@@ -101,9 +126,15 @@ from pygame import mixer
 # Centena: 8
 # Milhar: 1
 
+#___________________________________________________________________________________________________#
+
 # Crie um sistema que leia o nome de uma cidade e diga se ela começa ou não com a palavra SANTO.
 
+#___________________________________________________________________________________________________#
+
 #Crie um programa que leia um nome e diga se a pessoa tem SILVA no nome
+
+#___________________________________________________________________________________________________#
 
 # Crie um programa que leia uma frase e mostre:
 
@@ -112,6 +143,8 @@ from pygame import mixer
 # Em que posição ela aparece a primeira vez;
 #
 # Em que posição ela aparece a última vez;
+
+#___________________________________________________________________________________________________#
 
 #Faça um program que leia o nome de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente
 
--- Aulas/Usando Módulos/ManipulandoTextos.py
+++ Aulas/Usando Módulos/ManipulandoTextos.py
@@ -1,6 +1,61 @@
-#Fatiamento
-frase = "Curso em vídeo"
+#Fatiamento - Conseguir pegar pedaços de uma string
 
-# print(frase[5:])
+frase = "Curso em Vídeos Python"
+
+print(frase[9]) #Pegara a letra que está na 9ª posição
+
+print(frase[9:13]) #Pegara da 9ª letra até a 13ª letra da variável
+
+print(frase[9:21]) #Faz o mesmo que o de cima, perceberá que tem a mais e pegará o ultimo valor
+
+print(frase[9:21:2]) #Pegara da 9ª letra até a 21ª letra pulano de 2 em 2
+
+print(frase[:5]) #Pegará do início do caractere 0 e vai até a 5ª letra
+
+print(frase[15:]) # Partirá da 15ª letra até a última
+
+print(frase[9::3]) #Começa apartir da 9ª letra e vai até o final, pulando de 3 em 3
+
+#Análise - Saber as característica da string
+
+print(len(frase)) #Comprimento da variável = Quantidade de espaços, caracteres e etc
+
+print(frase.count('o')) #Conta quantas vezes aparece o "O" na variável
+print(frase.count('o', 0, 13)) #Conta do 0 ate o 12 e mostra quantos "O" existem dentro deste fatiamento
+
+print(frase.find('deo')) #Informa onde ele encontra o que é solicitado
+
+print(frase.find('android')) #Quando não acha ele retorna "-1"
+
+print('Curso' in frase) #Diz se existe o que está sendo procurado (Em boleano)
+
+#Transformação - Em via de regra uma lista de string é imutável.
+
+print(frase.replace('Python', 'Android')) #Substitui a palavra pela outra
+
+print(frase.upper()) #Retorna a string com as letras maiúsculas, deixando as maiúsculas e só mexendo nas minúsculas
+
+print(frase.lower()) #Mesmo da anterio porém ao contrário
+
+print(frase.capitalize()) #Deixa apenas a primeira letra maiúscula
+
+print(frase.title()) #Analisa quantas palavras tem através dos espaços e deixa a primeira letra de cada palavra em maiúscila
+
+frase2 = "  Aprenda Python  "
+
+print(frase2.strip()) #Retira os espaços a mais
+
+print(frase2.rstrip()) #Remove os espaços da direita
+print(frase2.lstrip()) #Remove os espaços da esquerda
+
+#Divisão - Dividir strings
+
+print(frase.split()) #Divide a string considerando os espaços. Gera uma lista em todas as palavras numa cadeia de caracteres e recria os índices
+
+#Junção - Junta as sting
+
+print('-'.join(frase)) #Junta todos os elementos de "frase" e vai usar "-" como o separador, pode ser usado em espaços em branco
+print(''.join(frase))
+
 
 
